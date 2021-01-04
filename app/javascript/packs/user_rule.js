83b676d0797f40c01f9475db679c299e67c7781b
sc30gsw <haidoushi875@gmail.com>
2021-01-05 07:58:58 +0900

    ユーザー登録のメッセージを修正

--- app/javascript/packs/user_rule.js
+++ app/javascript/packs/user_rule.js
@@ -3,12 +3,20 @@ function rule(){
   const password = document.getElementById("user_password");
   userId.addEventListener("click", () => {
     const userNameRule = document.getElementById("username-rule");
-    userNameRule.innerHTML = "半角英数字16文字まで"
+    const userMessage = "半角英数字のみ16文字以内"
+    const userHTML = `
+      <p>${userMessage}</p>`;
+    userNameRule.insertAdjacentHTML("afterend", userHTML);
+    userNameRule.remove();
   })
   
   password.addEventListener("click", () => {
     const passwordRule = document.getElementById("password-rule");
-    passwordRule.innerHTML = "半角英数字(両方含む)8文字以上"
+    const passwordMessage = "半角英数字(両方含む)8文字以上"
+    const passwordHTML = `
+      <p>${passwordMessage}</p>`;
+    passwordRule.insertAdjacentHTML("afterend", passwordHTML);
+    passwordRule.remove();
   })
 };
 window.addEventListener("load", rule);
