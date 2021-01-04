8ed818cc8d6d85fbbd408b60223ca8fadd2ade1b
sc30gsw <haidoushi875@gmail.com>
2021-01-04 18:48:10 +0900

    noteモデルの一時削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_103_003_602) do
-  create_table 'notes', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
+
 end
