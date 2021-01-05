f9ce76633a6e2a86f64ec02257b943bc8fad7975
sc30gsw <haidoushi875@gmail.com>
2021-01-06 06:40:26 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
     validates :nickname, format: { with: /\A[a-z\d]{4,16}+\z/i, message: "4~16文字以内の半角英数字ではありません"}
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i, message: "8文字以上の半角英数字ではありません" }
   end
+
+  has_many: notes
 end
