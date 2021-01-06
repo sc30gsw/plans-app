4b8c19ef8abe65fed1ecbbc7cabdf7173283a50b
sc30gsw <haidoushi875@gmail.com>
2021-01-06 16:34:31 +0900

    carrierwaveの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
     validates :nickname, format: { with: /\A[a-z\d]{4,16}+\z/i, message: 'が4文字以上の半角英数字ではありません' }
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i, message: 'は8文字以上の半角英数字ではありません' }
   end
+
+  mount_uploader :image, ImageUploader
 end
