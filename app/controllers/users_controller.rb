08fae6c85672fe5b8dc56716b679860df15a587f
sc30gsw <haidoushi875@gmail.com>
2021-01-06 20:11:21 +0900

    editとupdateアクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show; end
 
+  def edit
+  end
+
+  def update
+  end
+  
   private
 
   def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'notes#index'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
