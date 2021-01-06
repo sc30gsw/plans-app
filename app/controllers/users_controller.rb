83c49e1843ec22cb735e2b9fcd3a0bb04a0c6a56
sc30gsw <haidoushi875@gmail.com>
2021-01-06 16:38:14 +0900

    ユーザー編集と更新のアクションを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show; end
+
+  def edit
+  end
+
+  def update
+  end
 
   private
 
