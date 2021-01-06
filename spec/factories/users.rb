bfbd0e9da40ce3ee90edf0e39e422fe7489f119a
sc30gsw <haidoushi875@gmail.com>
2021-01-06 11:06:58 +0900

    nicknameを半角英数字のみに指定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname {'1a' + Faker::Name.name}
+    nickname {'1111test'}
     email {Faker::Internet.free_email}
     password{'1a' + Faker::Internet.password(min_length: 8)}
     password_confirmation {password}
