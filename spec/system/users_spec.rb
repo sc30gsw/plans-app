6d8b474e1527a1cbad5771ede000e81b986703e8
sc30gsw <haidoushi875@gmail.com>
2021-01-06 14:31:20 +0900

    ログイン機能の結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -24,6 +24,9 @@ RSpec.describe "ユーザー新規登録", type: :system do
       }.to change {User.count}.by(1)
       # トップページへ遷移することを確認する
       expect(current_path).to eq root_path
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されないことを確認する
+      expect(page).to have_no_content('ユーザー登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
 
@@ -46,6 +49,48 @@ RSpec.describe "ユーザー新規登録", type: :system do
       }.to change {User.count}.by(0)
       # 新規登録ページへ戻されることを確認する
       expect(current_path).to eq '/users'
+    end
+  end
+end
+
+RSpec.describe "ログイン", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインできるとき' do
+    it '保存されているユーザーの情報と合致すればログインできる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # 正しいユーザー情報を入力する
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されないことを確認する
+      expect(page).to have_no_content('ユーザー登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+  
+  context 'ログインできないとき' do
+    it '保存されているユーザーの情報と合致しないとログインできない' do
+      # トップぺージに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'user_email', with: ""
+      fill_in 'user_password', with: ""
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq new_user_session_path
     end
   end
 end
