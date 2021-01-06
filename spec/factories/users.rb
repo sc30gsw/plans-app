8ef6aa3dc4f187cc14a7d07a1ea7395b7172a23d
sc30gsw <haidoushi875@gmail.com>
2021-01-06 11:00:51 +0900

    記述の修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    nickname {'1a' + Faker::Name.name(min_length: 4)(max_length: 16)}
+    nickname {'1a' + Faker::Name.name(min_length: 4)}
     email {Fkaer::Internet.free_email}
     password{'1a' + Faker::Internet.password(min_length: 8)}
     password_confitmation {password}
