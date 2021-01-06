a7fad570ac347e68962853c3d91570563ccba609
sc30gsw <haidoushi875@gmail.com>
2021-01-06 22:42:59 +0900

    new・createアクションの設定

--- app/models/mypage.rb
+++ app/models/mypage.rb
@@ -1,2 +1,16 @@
 class Mypage < ApplicationRecord
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_mypage
+  end
+
+  def mypage_params
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'notes#index'
   resources :users, only: [:show]
+  resources :mypages, only: [:new, :create]
 end
