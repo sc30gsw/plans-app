b124437da7c1ff06e7cfceaa504b788baf87361d
sc30gsw <haidoushi875@gmail.com>
2021-01-06 14:17:09 +0900

    ユーザー新規登録の結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -8,22 +8,44 @@ RSpec.describe "ユーザー新規登録", type: :system do
   context 'ユーザー新規登録ができるとき' do
     it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ユーザー登録')
       # 新規登録ページに移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'user_nickname', with: @user.nickname
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      fill_in 'user_password_confirmation', with: @user.password_confirmation
       # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect {
+        find('input[name="commit"]').click
+      }.to change {User.count}.by(1)
       # トップページへ遷移することを確認する
+      expect(current_path).to eq root_path
     end
   end
 
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ユーザー登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'user_nickname', with: ""
+      fill_in 'user_email', with: ""
+      fill_in 'user_password', with: ""
+      fill_in 'user_password_confirmation', with: ""
       # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect {
+        find('input[name="commit"]').click
+      }.to change {User.count}.by(0)
       # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq '/users'
     end
   end
 end
