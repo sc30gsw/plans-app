d5d60279380bc537e494d691a06a4b7eba9ecd38
sc30gsw <haidoushi875@gmail.com>
2021-01-08 09:54:37 +0900

    sns_credentialモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_003435) do
+ActiveRecord::Schema.define(version: 2021_01_08_005315) do
 
   create_table "intros", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_01_07_003435) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_intros_on_user_id"
+  end
+
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -38,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_01_07_003435) do
   end
 
   add_foreign_key "intros", "users"
+  add_foreign_key "sns_credentials", "users"
 end
