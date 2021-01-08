dea96c51c12e9a954f2e6659828e298a9499260a
sc30gsw <haidoushi875@gmail.com>
2021-01-08 13:44:05 +0900

    sns認証を行ったか判断するため

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,12 +15,17 @@ class User < ApplicationRecord
   # メソッド定義時binding.pryでauthが取得できるか確認する
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
-
     # sns認証したことがあればアソシエーションで取得
-    # なければemailでユーザー検索して取得orビルド(保存しない)
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
     user = User.where(email: auth.info.email).first_or_initialize(
-      nickname: auth.info.name
-         email: auth.info.email
+      nickname: auth.info.name,
+        email: auth.info.email
     )
+    # userが登録済みであるか判断
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
   end
 end
