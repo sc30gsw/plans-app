db4f7e45dab3bce2c4cc3f538237891c414ce8f1
sc30gsw <haidoushi875@gmail.com>
2021-01-09 21:59:11 +0900

    notesテーブルの作成

--- db/migrate/20210109092218_create_notes.rb
+++ db/migrate/20210109092218_create_notes.rb
@@ -1,7 +1,10 @@
 class CreateNotes < ActiveRecord::Migration[6.0]
   def change
     create_table :notes do |t|
-
+      t.string :title, null: false
+      t.text :text, null: false
+      t.text :plan, mull: false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -45,8 +45,13 @@ ActiveRecord::Schema.define(version: 2021_01_09_115643) do
   end
 
   create_table "notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.text "plan"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -73,5 +78,6 @@ ActiveRecord::Schema.define(version: 2021_01_09_115643) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "intros", "users"
+  add_foreign_key "notes", "users"
   add_foreign_key "sns_credentials", "users"
 end
