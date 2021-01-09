503b6a82dfc2244e8a14632d16d8cb526c05ae50
sc30gsw <haidoushi875@gmail.com>
2021-01-09 21:59:37 +0900

    アクションの設定

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,25 @@
 class NotesController < ApplicationController
 
   def index
+    @notes = Note.all.order(id: 'DESC')
   end
 
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+    if @note.valid?
+      @note.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :text, :)
 end
