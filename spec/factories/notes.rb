6eaf17d2ce9e0217926b3a96c298c2ec3eaa6ec7
sc30gsw <haidoushi875@gmail.com>
2021-01-10 14:29:27 +0900

    noteモデルの単体テスト

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :note do
-    
+    title {Faker::Lorem.sentence}
+    text {Faker::Lorem.sentence}
+    plan {Faker::Lorem.sentence}
+    association :user
   end
 end
