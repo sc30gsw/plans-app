1f73919ed80611bdbf39e39a12f4c2f562fe40c1
sc30gsw <haidoushi875@gmail.com>
2021-01-10 19:37:09 +0900

    ゲストユーザーログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    post 'users/guest_sigin_in', to: 'users/sessions#new_guest'
+  end
   root to: 'notes#index'
   resources :users, only: [:show] 
   resources :intros, only: [:new, :create, :edit, :update]
