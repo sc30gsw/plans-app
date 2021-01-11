47a8f068db54d772058dc5475f0fe0252b9ec15d
sc30gsw <haidoushi875@gmail.com>
2021-01-12 06:05:14 +0900

    コメント送信機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:cooment).permit(:text, :image).merge(user_id: current_user.id, note_id: params[:note_id])
+    params.require(:comment).permit(:text, :image).merge(user_id: current_user.id, note_id: params[:note_id])
   end
 end
