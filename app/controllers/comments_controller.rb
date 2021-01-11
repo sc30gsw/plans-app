eee909cde9b0be40d905185e99b237cc9363e485
sc30gsw <haidoushi875@gmail.com>
2021-01-12 05:40:27 +0900

    ストロングパラメーターの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,10 @@ class CommentsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def comment_params
+    params.require(:cooment).permit(:text, :image).merge(user_id: current_user.id, note_id: params[:note_id])
+  end
 end
