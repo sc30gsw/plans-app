b490a9ca35ca3d981060ec2f2273e15d1781c0db
sc30gsw <haidoushi875@gmail.com>
2021-01-12 05:47:10 +0900

    コメントのアクションの設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.valid?
+      @comment.save
+      redirect_to note_path(@comment.note)
+    else
+      @note = @comment.note
+      @comments = @note.comments
+      render "notes/show"
+    end
   end
 
   private
