0c3d2f2fd188ed77be96498aecd6ae0aae01874c
sc30gsw <haidoushi875@gmail.com>
2021-01-12 11:49:19 +0900

    コメントモデルの単体テスト

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :comment do
-    
+    text { Faker::Lorem.sentence }
+
+    association :user
+    association :note
+
+    after(:build) do |comment|
+      comment.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント新規投稿' do
+    context 'コメント投稿がうまくいくとき' do
+      it 'textが存在すれば投稿できる' do
+        @comment.image = nil
+        expect(@comment).to be_valid
+      end
+
+      it 'imageが存在すれば投稿できる' do
+        @comment.text = nil
+        expect(@comment).to be_valid
+      end
+
+      it 'すべての項目が存在すれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメント投稿がうまくいかないとき' do
+      it 'textとimageの両方が空だと投稿できない' do
+        @comment.text = nil
+        @comment.image = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Textを入力してください")
+      end
+    end
+  end
 end
