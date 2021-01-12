b206b4cc4085b5ba75c1361408826e08d411a559
sc30gsw <haidoushi875@gmail.com>
2021-01-12 19:16:01 +0900

    やり直しのため削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_203054) do
+ActiveRecord::Schema.define(version: 2021_01_12_075230) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,15 @@ ActiveRecord::Schema.define(version: 2021_01_11_203054) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["note_id"], name: "index_comments_on_note_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "note_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_favorites_on_note_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "intros", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -89,6 +98,8 @@ ActiveRecord::Schema.define(version: 2021_01_11_203054) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "notes"
   add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "notes"
+  add_foreign_key "favorites", "users"
   add_foreign_key "intros", "users"
   add_foreign_key "notes", "users"
   add_foreign_key "sns_credentials", "users"
