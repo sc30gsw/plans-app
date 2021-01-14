d70b0f5438b36c37d0ba3c034b6583ef64b21a3d
sc30gsw <haidoushi875@gmail.com>
2021-01-14 16:13:18 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,15 @@ class User < ApplicationRecord
   has_one :intro
   has_many :sns_credentials
   has_many :notes
+  has_one_attached :image
   has_many :comments
+  has_many :commented_notes, through: :comments, source: :note
   has_many :favorites, dependent: :destroy
   has_many :favorited_notes, through: :favorites, source: :note
-  has_many :commented_notes, through: :comments, source: :note
-  has_one_attached :image
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
