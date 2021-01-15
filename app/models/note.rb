5ded43e71a31a7c13ed9b79de7a23cb1fa89c69f
sc30gsw <haidoushi875@gmail.com>
2021-01-15 14:22:32 +0900

    アソシエーションの設定

--- app/models/note.rb
+++ app/models/note.rb
@@ -9,6 +9,7 @@ class Note < ApplicationRecord
   has_one_attached :image
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
+  has_many :tags, through: :note_tags
 
   # コメントした投稿を取得するための記述
   has_many :commented_users, through: :comments, source: :user
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :notes, through: :note_tags
+  has_many :note_tags
 end
