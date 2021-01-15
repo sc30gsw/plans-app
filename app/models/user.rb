833862de0cabddf3ec1062c9197b138fdf92ebf0
sc30gsw <haidoushi875@gmail.com>
2021-01-15 09:14:50 +0900

    記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -79,7 +79,7 @@ class User < ApplicationRecord
 
   # フォロー通知
   def create_notification_follow!(current_user)
-    temp = Notification.where(["viditer_id = ? and visited_id = ? and action = ?", current_user.id, id, 'follow'])
+    temp = Notification.where(["visiter_id = ? and visited_id = ? and action = ?", current_user.id, id, 'follow'])
     if temp.blank?
       notification = current_user.active_notifications.new(
         visited_id: id,
