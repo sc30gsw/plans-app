43e540f065f281ff33998e8515445910badc0bf5
sc30gsw <haidoushi875@gmail.com>
2021-01-15 17:01:18 +0900

    dependentの追加

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,8 +4,8 @@ class Note < ApplicationRecord
   has_one_attached :image
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
-  has_many :note_tags
-  has_many :tags, through: :note_tags
+  has_many :note_tags, dependent: :destroy
+  has_many :tags, through: :note_tags, dependent: :destroy
 
   # コメントした投稿を取得するための記述
   has_many :commented_users, through: :comments, source: :user
