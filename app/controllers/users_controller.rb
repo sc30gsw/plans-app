aa2efa62bcea76e39491e0d7438ceb6ce1783805
sc30gsw <haidoushi875@gmail.com>
2021-01-18 05:57:00 +0900

    ページネーション表示の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
-    @notes = @user.notes.page(params[:page]).per(5)
+    @notes = @user.notes.page(params[:page]).per(4)
     @status_notes = @user.notes.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
     @all_count = @status_notes.count
     @favorited_notes = @user.favorited_notes.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
