806da0d18c9337e1681c39cdf8e31df84e1e842b
sc30gsw <haidoushi875@gmail.com>
2021-01-18 05:52:50 +0900

    ユーザーの1週間分のアナリティクスの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
 
   def show
     @notes = @user.notes.page(params[:page]).per(5)
+    @status_notes = @user.notes.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
+    @all_count = @status_notes.count
+    @favorited_notes = @user.favorited_notes.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
+    @commented_notes = @user.commented_notes.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
   end
 
   private
