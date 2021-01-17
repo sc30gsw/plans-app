5af299f60b750c15de06098b8b8b53f2d1512691
sc30gsw <haidoushi875@gmail.com>
2021-01-17 14:52:39 +0900

    test

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -54,3 +54,9 @@
 #     # password: "please use keys"
 #   }
 server '52.196.43.194', user: 'ec2-user', roles: %w[app db web]
+
+set :ssh_options, {
+  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  forward_agent: true,
+  auth_methods: %w[publickey]
+}
