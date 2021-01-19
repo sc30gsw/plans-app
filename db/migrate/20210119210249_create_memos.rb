c169bffed051509a74ce1a3c64b45e8dee3751fb
sc30gsw <haidoushi875@gmail.com>
2021-01-20 06:05:34 +0900

    テーブルの作成

--- db/migrate/20210119210249_create_memos.rb
+++ db/migrate/20210119210249_create_memos.rb
@@ -1,7 +1,9 @@
 class CreateMemos < ActiveRecord::Migration[6.0]
   def change
     create_table :memos do |t|
-
+      t.text :content, null: false
+      t.boolean :checked
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
