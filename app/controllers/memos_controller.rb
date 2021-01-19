162d044a65007d0e6e4dd376cb8eed43a38f973e
sc30gsw <haidoushi875@gmail.com>
2021-01-20 06:30:17 +0900

    アクション内の処理の設定

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,11 +1,28 @@
 class MemosController < ApplicationController
 
   def index
+    @memos = Memo.all.order('created_at DESC')
   end
 
   def create
+    @memo = Memo.create(memo_params)
+    @memo.save
+    redirect_to "/memos/user/#{current_user.id}"
+    # render json:{ post: post }
   end
 
   def checked
+    memo = Memo.find(parma[:id])
+    if memo.checked
+      memo.update(checked: false)
+    else
+      memo.update(checked: true)
+    end
+  end
+
+  private
+
+  def memo_params
+    params.permit(:content).merge(user_id: current_user.id)
   end
 end
