f2375ad177929a7c24036ad00f212e79cd693c91
sc30gsw <haidoushi875@gmail.com>
2021-01-20 12:22:00 +0900

    DMの保存の記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,31 @@ class UsersController < ApplicationController
 
   def show
     @notes = @user.notes.page(params[:page]).per(4)
+
+    # ユーザーnalytics
     @status_notes = @user.notes.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
     @all_count = @status_notes.count
     @favorited_notes = @user.favorited_notes.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
     @commented_notes = @user.commented_notes.where(created_at: 6.days.ago.beginning_of_day..Time.zone.now.end_of_day)
+
+    # DM機能
+    @currentUserEntry = Entry.where(user_id: current_user.id)
+    @userEntry = Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @userEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   private
