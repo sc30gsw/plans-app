365d517ab93e0ffe03e0ffdf5dab972de8eb708a
sc30gsw <haidoushi875@gmail.com>
2021-01-20 10:45:39 +0900

    ドラッグ&ドロップの実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("./user_rule")
 require("./user_icon")
 require("./tag")
 require("./checked")
+require("./drag")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
