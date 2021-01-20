b333c873bc8e66b79d18332fd9b2f592f9422a0d
sc30gsw <haidoushi875@gmail.com>
2021-01-20 12:22:12 +0900

    保存と詳細の記述

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,30 @@
 class RoomsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_room, only: [:show, :destroy]
+
+  def create
+    @room = Room.create
+    @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
+    @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
+    redirect_to "/rooms/#{@room.id}"
+  end
+
+  def show
+    if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.messages
+      @message = Message.new
+      @entries = @room.entries
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
 end
