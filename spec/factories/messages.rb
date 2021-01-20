9966088be673c457224b84cfdac58fbab4641867
sc30gsw <haidoushi875@gmail.com>
2021-01-20 16:23:57 +0900

    Messageモデルの単体テスト

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :message do
+    text {Faker::Lorem.sentence}
     
+    association :user
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'メッセージ投稿' do
+    context 'メッセージ投稿がうまくいかないとき' do
+      it 'textが空だと投稿できない' do
+        @message.text = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include('Textを入力してください')
+      end
+
+      it 'roomが空だと投稿できない' do
+        @message.room_id = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Roomを入力してください")
+      end
+    end
+  end
 end
