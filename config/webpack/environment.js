54c0eafacd6b3293a906fe5c7b631e43765ce34f
sc30gsw <haidoushi875@gmail.com>
2021-01-25 06:33:46 +0900

    修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
@@ -9,7 +7,3 @@ environment.plugins.prepend('Provide',
         jQuery: 'jquery/src/jquery'
     })
 )
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-module.exports = environment
